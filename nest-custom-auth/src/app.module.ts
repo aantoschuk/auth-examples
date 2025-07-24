090be78437c231cd9ad7384e1fa4319f253726a5
Alexander Antoschuk <alexander.antoschuk.dev@gmail.com>
2025-07-24 11:03:01 +0300

    nest-custom-auth/config-object

--- nest-custom-auth/src/app.module.ts
+++ nest-custom-auth/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppService } from './modules/app/app.service';
 import { AppController } from './modules/app/app.controller';
 
+import configuration from './config/configuration';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({load: [configuration] })],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-custom-auth/src/main.ts
+++ nest-custom-auth/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import configuration from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = configuration().port;
+
+  await app.listen(port);
 }
 bootstrap();
